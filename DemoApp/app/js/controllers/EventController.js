a070199f49de504523b648add3c88bab3e46b384
Cubelaster <david.cubela@gmail.com>
2016-07-06 14:02:28 +0200

    Anchor Scroll showcase
    Just a little demonstration of anchor scroll
    Works, but not really how it should

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData) {
+    function EventController($scope, eventData, $anchorScroll) {
 
         $scope.sortOrder;
         $scope.query;
@@ -20,5 +20,16 @@ eventsApp.controller('EventController',
 
         $scope.downVoteSession = function (session) {
             session.upVoteCount--;
+        }
+
+        $scope.scrollToSession = function() {
+            $anchorScroll();
+            /* this anchorScroll does nothing without the ngRoute.
+             However, if we input the hash in the link, it will do 
+             it's job. The hash link is determined by id. 
+             In this case the id will be generated in the repeater
+             and look like session_{{number}}. The link will then 
+             look like {{rootLink}}+##id. 
+            */
         }
     });
