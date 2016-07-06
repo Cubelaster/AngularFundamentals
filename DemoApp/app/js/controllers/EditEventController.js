0febd4f3482188ed01382d43ca9544326bc20502
Cubelaster <david.cubela@gmail.com>
2016-07-06 09:23:32 +0200

    ValidationComplete
    $dirty
    $valid
    Wrapped the event in validation
    Define validation parameters

--- DemoApp/app/js/controllers/EditEventController.js
+++ DemoApp/app/js/controllers/EditEventController.js
@@ -2,7 +2,8 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
-        $scope.saveEvent = function (event) {
+        $scope.saveEvent = function (event, newEventForm) {
+            console.log(newEventForm);
             if (newEventForm.$valid) {
                 window.alert('Event ' + event.name + ' saved!');
             }
