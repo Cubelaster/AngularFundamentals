8e4ea00ed941de7c190c45971273c295f49ecf4f
Cubelaster <david.cubela@gmail.com>
2016-07-14 12:13:21 +0200

    Linked directories
    Added attributes to greeting directory
    Linked them to 1 controller
    Added priorities and terminal

--- DemoApp/app/js/directives/greeting.js
+++ DemoApp/app/js/directives/greeting.js
@@ -1,23 +1,61 @@
 'use strict';
 
-eventsApp.directive('greeting', function () {
-    return {
-        restrict: 'E',
-        replace: true,
-        template: "<button class='btn' ng-click='sayHello()'>Say hello</button>",
-        controller: '@', // transports reference
-        name: 'ctrl' // points to attribute in html
-        // 'GreetingController' //No2  
-        // function($scope) {
-        //     $scope.sayHello = function () {
-        //         alert('Hello!');
-        //     }
-        // } // this is an example of controller defined in the same line No1
-    };
-});
+eventsApp
+    .directive('greeting', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            priority: -1,
+
+            template: "<button class='btn' ng-click='sayHello()'>Say hello</button>",
+            // controller: '@', // transports reference //No2
+            // name: 'ctrl' // points to attribute in html //No2
+            controller: 'GreetingController' //No2  
+            // controller: function ($scope) {
+            //     var greetings = ['Hello!'];
+
+            //     $scope.sayHello = function () {
+            //         alert(greetings.join());
+            //     }
+            //     this.addGreeting = function (greeting) {
+            //         greetings.push(greeting);
+            //     }
+            // } // this is an example of controller defined in the same line No1
+        };
+    })
+    .directive('finnish', function () {
+        return {
+            restrict: 'A',
+            priority: -1,
+            terminal: true, 
+            // terminal says that when this event is done, nothing else happens
+            // In this case, if finnish executes, hindi won't 
+            // BUT using terminal requires you to use negative priority values
+            require: 'greeting',
+            link: function (scope, element, attr, controller) {
+                controller.addGreeting('hei');
+            }
+        }
+    })
+    .directive('hindi', function () {
+        return {
+            restrict: 'A',
+            priority: -2,
+            require: 'greeting',
+            link: function (scope, element, attr, controller) {
+                controller.addGreeting('heil');
+            }
+        }
+    });
 
 eventsApp.controller('GreetingController', function GreetingController($scope) {
+    var greetings = ['Hello!'];
+
     $scope.sayHello = function () {
-        alert('Hello');
+        alert(greetings.join());
+    }
+    this.addGreeting = function (greeting) {
+        // for some reason it's THIS instead of $scope
+        greetings.push(greeting);
     }
 }); // this is an example of a normal controller simply included in directive //No2
