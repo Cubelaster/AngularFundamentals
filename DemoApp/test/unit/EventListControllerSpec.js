557bb556ce3351d0c9f9043e94786dc79bfbd23a
Cubelaster <david.cubela@gmail.com>
2016-07-15 15:35:06 +0200

    Revert "Karma Test Environment Set Up"
    This reverts commit 4ef235c9eea81509e8990ada15ad9fb455674dec.

--- DemoApp/test/unit/EventListControllerSpec.js
+++ DemoApp/test/unit/EventListControllerSpec.js
@@ -13,7 +13,6 @@ describe('EventListController', function() {
 
   it('should stuff', function() {
     var mockEvents = {};
-    
     mockEventData.getAllEvents.returns(mockEvents);
 
     var ctrl = $controllerConstructor("EventListController",
